b111ce10699093165a0e731345b0163ced5e3d4d
Bang Pham <bangpham@Bangs-MBP.attlocal.net>
2024-10-14 16:46:27 -0400

    Ui for static card. Hard code flip

--- card_matching/lib/main.dart
+++ card_matching/lib/main.dart
@@ -20,10 +20,63 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.pink,
         ),
         body: const Padding( padding:  EdgeInsets.all(16),
-          child: Text('Hello World'),
+          child:GameCard( isFlipped: false, icon: 'https://pngimg.com/uploads/pumpkin/pumpkin_PNG86730.png',),
         ),
       ),
     );
   }
 }
 
+class GameCard extends StatefulWidget {
+  
+  final bool isFlipped;
+  final String icon;
+  const GameCard({super.key, required this.isFlipped, required this.icon});
+
+
+  @override
+  _GameCardState createState() => _GameCardState();
+}
+
+class _GameCardState extends State<GameCard> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black.withOpacity(.05), width: 1),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.pink.withOpacity(0.2),
+            spreadRadius: 0,
+            blurRadius: 30,
+            offset: const Offset(0, 15),
+          ),
+        ],
+      ),
+      child: Center(
+        child: widget.isFlipped
+            ? const Icon(
+                Icons.star, // Placeholder icon for the card back
+                size: 50,
+                color: Colors.pink,
+              ) // Display the image when flipped
+            : const Icon(
+                Icons.question_mark, // Placeholder icon for the card back
+                size: 50,
+                color: Colors.pink,
+              ), 
+      ),
+      //add button to flip the card
+      
+    );
+  }
+    
+  }
+
+ 
